4b2590c91c8831b7c34a293272cfbb45bd20f8de
Linda Smith <lindasmith@Lindas-MacBook-Pro.local>
2018-04-08 15:22:34 +0100

    Work on multiroutes

--- multiroutes.py
+++ multiroutes.py
@@ -4,11 +4,12 @@ import operator
 import argparse
 import geopy.distance
 from operator import itemgetter
-
+import math
+import itertools
 
 #Taking input of destinations from user in the form of one long string
 #To input a chain of destinations/run program type:
-#multiroutes.py -l "DUB SYD GBK QTN"
+#multiroutes.py -l "DUB LDN SYD AZ3 KDH"
 #Gets converted into a list
 #need to iterate through list and for each item create a new dictionary with corresponding latitude and longitide
 
@@ -36,27 +37,21 @@ for row in sort1:
             #distances == airports[airport_id]
             #print(distances)
 
+#print(distances)
+
 #append values into list ==> then have group of values which comprise coords- latitude and longitude
 
-print(distances)
-
 list2 =[]
-
 for key, value in distances.items():
     list2.append(value)
 
-# print(list2)
+#Sort elements in middle of list by 'sorted' function => sorts based on increasing latitude
 
-# origin = list2[0]
-# home = list2[-1]
-# print(origin)
-# print(home)
+list2[1:-1]= sorted(list2[1:-1])
+print(list2)
 
-
-#Sort elements in middle of list by 'sorted' function => sorts based on latitude
-sorted_dests = sorted(list2, key=itemgetter(slice(1,-1)))
-print(sorted_dests)
-
+#if distance between element[0] and element[1] > element[0] and element[2]
+#swap values
 
 #Next step is to calculate distance between each element in sorted_dests list and apply selection sort algorithm
 def distanceBetweenAirports(coords_1, coords_2):
@@ -66,12 +61,97 @@ def distanceBetweenAirports(coords_1, coords_2):
 	return geopy.distance.vincenty(coords_1, coords_2).km
 
 
+def distance(p0, p1):
+    return math.sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
 
-# ---------------------------------------------
-# print("Unsorted list distances")
-# print(distanceBetweenAirports(origin, list2[1]))
-# print(distanceBetweenAirports(list2[1],list2[2]))
-# print(distanceBetweenAirports(list2[2], home))
+# min_distance = distance(list2[0], list2[1])
+# for p0, p1 in itertools.combinations(list2, 2):
+#     min_distance = min(min_distance, distance(p0, p1))
+#     print(min_distance)
+
+min_distance = distanceBetweenAirports(list2[0], list2[1])
+for p0, p1 in itertools.combinations(list2, 2):
+    distance_btw_airports = distanceBetweenAirports(p0, p1)
+    #min_distance = min(min_distance, distanceBetweenAirports(p0, p1))
+    #print("Distance between:", p0, p1, "is", distance_btw_airports)
+    # if distance_btw_airports(p0, p1) > distance_btw_airports(p0, p1):
+    #     p0,p1 = p0,p1
+    #print("Distance between:",p0, p1, "is", min_distance)
+
+min_distance = distanceBetweenAirports(list2[1], list2[2])
+for p0,p1 in itertools.combinations(list2, 2):
+    distance_btw_airports = distanceBetweenAirports(p0, p1)
+    print("Distance between:", p0, p1, "is", distance_btw_airports)
+
+
+
+def bestRoutes(list):
+    """Calculates best routes from list of input"""
+    #From user input as string of aircodes, aircodes are parsed into a list
+    #next the middle elements of list are sorted 
+    #iterates through sorted list calculating distance between each element
+    #sums distance between each element
+    #multiplies distance by cost to get total journey cost
+    #iterate through sorted list to find corresponding lat and long in distances dict:
+    #when found return the aircodes in order of least distance
+
+
+
+
+
+
+
+def insertion_sort(list):
+    for index in range(1,len(list)):
+        value = list[index]
+        i = index -1
+        for p0, p1 in itertools.combinations(list, 2):
+            if distanceBetweenAirports(p0, p1) < distanceBetweenAirports(p0, p1):
+                list[i+1] = list[i]
+                list[i] = value
+
+print(insertion_sort(list2))
+
+        # while i>=0:
+        #     if value < list[i]:
+        #         list[i+1] = list[i]
+        #         list[i] = value
+
+
+
+
+# min_distance = distanceBetweenAirports(sorted_dests[0], sorted_dests[1])
+# #iterates over sorted_dests list 
+# for p0, p1 in itertools.combinations(sorted_dests,2):
+#     min_distance = min(min_distance, distanceBetweenAirports(p0, p1))
+# print(min_distance)
+#     print(distanceBetweenAirports(sorted_dests[0], sorted_dests[1]))
+    
+#print(dist_btwn)
+# print(list2)
+# origin = list2[0]
+# home = list2[-1]
+# print(origin)
+# print(home)
+
+
+
+
+
+
+
+# for element in sorted_dests:
+#     print(distanceBetweenAirports(element[0], element[1]))
+
+# print(distanceBetweenAirports())
+
+    #for i in sorted_dests:
+        # if distanceBetweenAirports(sorted_dests[0], sorted_dests[2]) > distanceBetweenAirports(sorted_dests[], sorted_dests[i+2]):
+            # sorted_dests[i] == sorted_dests[i+1]
+
+#     sorted_dests[1] == sorted_dests[2]
+# print("Distance")
+#print(sorted_dests)
 
 
 # def selection_sort(items):
@@ -80,11 +160,13 @@ def distanceBetweenAirports(coords_1, coords_2):
 #    # Find the location of the smallest element in list
 #     for step in range(len(items)):
 #         closest_location = step
-#         for i in range(step, len(list2)):
-#             if distanceBetweenAirports(list2[i],list2[1]) < distanceBetweenAirports(list2[1],list2[2]):
+#         for i in range(step, len(items)):
+#             if distanceBetweenAirports(list2[i],list2[1]) > distanceBetweenAirports(list2[1],list2[2]):
+#                 list2[1] = list2[2]
 #                 closest_location = step
 #                 temporary_item = items[step]
 #         items[step] = items[closest_location]
 #         items[closest_location] = temporary_item
 
-# print(selection_sort(list2))
+# print(selection_sort(sorted_dests))
+
