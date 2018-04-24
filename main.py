d26e82efd5e372a59c760644ba2885cfb92cc12d
Linda Smith <lindasmith@dhcp-c1018274.ucd.ie>
2018-04-24 12:09:04 +0100

    updates -algorithms

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ from collections import Counter
 from utils import utils
 from currency import Currency
 from airport import Airport
+from dijkstra_github import Graph
 import geopy.distance
+from bfs2 import bfs
+
 
 
 def main():
@@ -18,15 +21,16 @@ def main():
     testCsv = _utObject.input(options)
     _airdict = Airport()
     parsedAirportDict = _airdict.parseAirport('airport.csv')
-
     
     for row in testCsv:
         distances = {}
         for i in row:
             data = parsedAirportDict.get(i)
             distances[i] = data
-        # print(distances)
+        #print(distances)
 
+
+    #list of distances
     finDist = []
     for airs in distances:
         src = airs
@@ -39,22 +43,77 @@ def main():
     print(finDist)
 
 
+    #To make dictionary of distances
+    dict_air = {}
+    for airs in distances:
+        src = airs
+        indivDist = []
+        for airs2 in distances:
+            dest = airs2
+            indivDist.append(_airdict.distanceBetweenAirports(distances[src][0], distances[src][1], distances[dest][0], distances[dest][1]))
+        dict_air[src] = indivDist
+    print(dict_air)
+    
+    #nodes are list of keys 
+
+    graph_nodes = (list(dict_air.keys()))
+    #print(graph_nodes)
+
+    g = Graph()
+    #adding keys of dict to become the nodes in my graph
+    for node in graph_nodes:
+        g.add_node(node)
+    g.add_edge('A', 'B', 10)
+    g.add_edge('A', 'C', 20)
+    g.add_edge('B', 'D', 15)
+    g.add_edge('C', 'D', 30)
+    g.add_edge('B', 'E', 50)
+    g.add_edge('D', 'E', 30)
+    g.add_edge('E', 'F', 5)
+    
+    #print(shortest_path(graph, 'A', 'D'))
+
+    # _bfsobject = bfs()
+    # bfs(finDist,finDist[0])
+
+
+
+
+    # g.add_edge(node[0], node[-1], 15937.30996897588)
+    # g.add_edge(node[0], node[-2], 11943.340591555452)
+
+    # print(g, node[0], node[-1])
+
+
+    # for i in dict_air:
+    #     x = (dict_air[i])
+    #     for k in x:
+    #         print(k)
+            #graph.add_edge(k)
+
     
 
 
+
+
+  
+if __name__ == "__main__":
+        main()
+
+
+
+    #Instead of having list of lists: have a dictionary where looks like:
+    # DUB: [123,123,124]
+    #SYD: [144, 444, 223]
+    #AUD: [123,233,222]
     
-        #data is storing distances 
-           # _airdict.distanceBetweenAirports(data[0],data[1])
-        
+
+
+
 
     # _currencyobject = Currency()
     # parsedCurrencyCost = _currencyobject.currencyParser('Currency_code_concat')
 
-
-    #print(dist_object.distanceEdges(testCsv))
-
-    # for i in v:
-    #     print(i)
 
     # distance = airport.Airport()
     
@@ -70,9 +129,7 @@ def main():
 
     
 
-if __name__=="__main__":
-    main()
-
+    
 
 
 
