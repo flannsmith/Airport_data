fa5da7d1acb1f75ca0ea27c4414049771fff7b8f
flannsmith <35727141+flannsmith@users.noreply.github.com>
2018-05-01 12:10:33 +0100

    Tidying main comments

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ def main():
         print("Checking if Itinerary is possible with provided aircraft")
         journeyPossible = _airdict.isItineraryPossible(aircraftRange[tCi],min_dist)
         if journeyPossible==False:
-            print("Sorry with the provided aircraft this journey cannot be made. Try again!")
+            print("Sorry, with the provided aircraft this journey cannot be made. Try again!")
             airports_visited.append("No Route")
             outputList.append(airports_visited)
         else:
@@ -124,8 +124,8 @@ def main():
             totalCosttoRefuel = 0
             toEuroRate = _costObject.toEuroRate(
                 airports_visited[0], 'airport.csv', 'countrycurrency.csv', 'currencyrates.csv')
-            print("\t\t\t\t\tItinerary Scheduled:::  ",airports_visited)
-            print("\t\t\t\t\tItinerary Scheduled:::  ", min_dist)
+            print("\t\t\t\t\tItinerary Scheduled:  ",airports_visited)
+            print("\t\t\t\t\tItinerary Scheduled:  ", min_dist)
             totalCosttoRefuel += aircraftRange[tCi]*toEuroRate
             for i,val in enumerate(min_dist):
                 toEuroRate = _costObject.toEuroRate(
@@ -149,110 +149,6 @@ def main():
 
     print("Generating output csv file")
     _utObject.tocsv(outputList)
-    #For the cost do I have to calculate the cost from last destination to home as well?? 
-
-    #why is my values saved in a tuple??? 
-   # for key in parsedCurrencyCost.items():
-        #print(key[0])
-        #if billingCountry == key[0]:
-           
-        # print(key[0])
-        # data = parsedCurrencyCost.get(billingCountry)
-        #print(data)
-        
-        
-        #if key == billingCountry:
-  
-
-
-        # 
-        #     print(value)
-    
-    # for key,value in parsedCurrencyCost.items():
-    #     #print(key,value)
-    #     #print(parsedCurrencyCost[key])
-    #     print(parsedCurrencyCost[value])
-    #     if airports_visited[-1] == parsedCurrencyCost[key]:
-    #         print(parsedCurrencyCost[value])
-    #         #print(key[1])
-
-  
-
-    #if sum shortest_path is greater than aircraft range, aircraft range minus sum and print out
-    #please refuel in ......... kms
-   
-   
-    #print(parsedCurrencyCost)
-
-  
-
-
-
- 
-
-
-    
-    # #sequences implement
-        
-    # #min_dist = min_dist[:-1]
-    # #print(airports_visited)
-    # newSrc = airports_visited[-1]
-    # home = airports_visited[0]
-    # airports_visited.append(home)
-    # #print(airports_visited)
-    # min_dist.pop()
-    # print(min_dist)
-
-    #for iata in currencydict:
-    #if code is there return to euro cost and save to list
-
-    
-    
-
-
-#print(key, 'is the key for the value', value)
-
-    
-
-
-
-
-
-
-          
-
-            
-
-
-            # parsedAircraftDict = _airdict.Aircraft('aircraft.csv')
-            # if aircraft_code in parsedAircraftDict:
-            #     print(parsedAircraftDict.get(aircraft_code))
-
-
-
-
-            #print(parsedAircraftDict)
-
-
-            #check aircraft input and save it to return range from key
-            #check key value pairs 
-
-            #and print(min(element))
-
-    
-
-        # if element != 0 
-        #     print(element)
-   #find the minimum value in the list, append it to min_dist
-
-   
-
-
-
-
-
-    # for key in dict_air:
-    #     print(dict_air.get(key))
 
 
 
@@ -260,18 +156,3 @@ if __name__ == "__main__":
     main()
 
 
-    #print(graph_nodes)
-
-
-    #List = airports visitied
-
-    #check 0
-    #is aircraft range below this
-    #else cannot do trip
-    #minimum value
-    #index of value
-
-    #List = Airports Visited
-    #airports_visited = []
-#     new_source = dict_air #key[0] return key of dict
-#     Distances = Get[]
